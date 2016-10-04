10cb17161efeb3e723b39d43c2e4a10d0a17e851
root <root@ds220>
2016-10-04 08:23:41 +0000

    modified bash file and deleted redundant files

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@ echo "--------------------------------------------------------------------------
 python mergeCWD.py
 python mergeItemStock.py
 python mergeOrders.py
+cqlsh -e "copy killrvideo.randoms(id,name) from 'test.csv' with header = false;"
 
 echo "Data inserted Successfully"
 echo "-----------------------------------------------------------------------------------------------"
