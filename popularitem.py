35e3e6a741ab212b0e51934a5c76f342c9062f49
root <root@ds220>
2016-10-05 05:50:26 +0000

    add popularitem.py file

---
+++ popularitem.py
@@ -0,0 +1,26 @@
+class PopularItem:
+	
+	def __init__(self,oid,item,quantity,entry,cid):
+		self.oid = oid
+		self.item = item
+		self.quantity = quantity
+		self.entry = entry
+		self.cid = cid
+
+
+class ItemInfo:
+
+	def __init__(self,item,name,count):
+		self.item = item
+		self.name = name
+		self.count = count
+
+class NewOrder:
+
+	def __init__(self,itemid,name,sw_id,quantity,amount,stock):
+		self.itemid = itemid
+		self.name = name
+		self.sw_id = sw_id
+		self.quantity = quantity
+		self.amount = amount
+		self.stock = stock
